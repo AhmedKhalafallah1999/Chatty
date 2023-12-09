70b44fd2a7e479a8fc681112a6fff465bd69ac3b
Ahmed Khalaf <ahmed1999khalafallah@gmail.com>
2023-12-09 19:33:16 +0200

    Featured socket io

--- client/src/components/Drawer.jsx
+++ client/src/components/Drawer.jsx
@@ -19,14 +19,50 @@ import { useChattyContext } from "../pages/Home";
 import DarkModeIcon from "@mui/icons-material/DarkMode";
 import LightModeIcon from "@mui/icons-material/LightMode";
 import { useAppContext } from "../App";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const DrawerHeader = styled(Toolbar)(() => ({
   justifyContent: "flex-end",
 }));
 
 const DrawerSlider = ({ isSmall, theme, isBig }) => {
-  const { sideBarOpen, ModalState, sideBarHandlerClose } = useChattyContext();
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const { sideBarOpen, ModalState, sideBarHandlerClose, socket } =
+    useChattyContext();
   const { lightThemeHandler, mode, darkThemeHandler } = useAppContext();
+  useEffect(() => {
+    const fetchCurrentUser = async () => {
+      const response = await fetch("/api/v1/feed/current-user");
+      const result = await response.json();
+      if (response.ok) {
+        socket.emit("associated-current-user", {
+          currentUserId: result.currentUserId.userId,
+        });
+      } else {
+        toast.error(result.msg);
+        return navigate("/login");
+      }
+    };
+    fetchCurrentUser();
+  }, [socket, navigate]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const response = await fetch("/api/v1/feed/all-users");
+      const result = await response.json();
+      console.log(result);
+      if (response.ok) {
+        console.log(result);
+        setUsers(result);
+      }
+    };
+    fetchUsers();
+  }, []);
+  const openChatTogetherHandler = (_Id) => {
+    socket.emit("chatWith", { chatWithUserId: _Id });
+  };
 
   return (
     <Drawer
@@ -84,24 +120,21 @@ const DrawerSlider = ({ isSmall, theme, isBig }) => {
       </List>
       <Divider />
       <List>
-        {[
-          "Ahmed Khalafallah",
-          "Hamdy Abdelsabour",
-          "Mohamed Tawheed",
-          "Eslam Fares",
-          "Mohamed Elnady",
-          "Mahmoud Elgamal",
-          "Marawan Hagag",
-          "Mahmoud Abdelsamea",
-          "Ahmed Ebrahim",
-          "Gerges",
-          "Ahmed Salah",
-        ].map((name, index) => {
+        {users.map((user, index) => {
           return (
             <ListItem key={index}>
-              <ListItemButton>
-                <ListItemIcon>{<AccountCircleIcon />}</ListItemIcon>
-                <ListItemText primary={name} />
+              <ListItemButton onClick={() => openChatTogetherHandler(user._id)}>
+                <ListItemIcon>
+                  {" "}
+                  {/* <img
+                    src={`data:image/svg+xml;utf8,${encodeURIComponent(
+                      user.avatarSrc
+                    )}`}
+                    alt={`Avatar ${index}`}
+                  /> */}
+                  <AccountCircleIcon />
+                </ListItemIcon>
+                <ListItemText primary={user.userName} />
               </ListItemButton>
             </ListItem>
           );
